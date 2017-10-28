ac5b7ddd3b15badb4017181f1ed02511295ceedf
Georgios Chairepetis <chaireg0@localhost.localdomain>
2017-10-28 22:55:08 +0100

    created contract

--- truffle.js
+++ truffle.js
@@ -4,6 +4,12 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      from: "0x0D030239b2116B2F23b44E1208e02edf67Cf9dF1",
+      network_id: "3"
     }
   }
 };
